46796db503206bd8ec615cf5f9ddbb254b6a7464
odalisjoaquina <odalis.joaquina@gmail.com>
2023-01-24 15:07:01 -0600

    agregar

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'song 1',
+        path: 'music/song 1.mp3',
+        artist: 'artist 1',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'music/song 2.mp3',
+        artist: 'artist 2',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'music/song 3.mp3',
+        artist: 'artist 3',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'music/song 4.mp3',
+        artist: 'artist 4',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'music/song 5.mp3',
+        artist: 'artist 5',
+        cover: 'images/cover 5.png'
+    },
+]
